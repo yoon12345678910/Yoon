33a109653c27e02d0e81067e0848b1d0f40cf700
jiyeong <jiyeong.kim5840@gmail.com>
2014-10-13 12:53:11 +0900

    dd

---
+++ java01/src/net/java63/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package net.java63.test;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("나의 첫번째 자바프로그램");
+  }
+
+}
