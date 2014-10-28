445b2111abb752cde32cd9961a8b572de69988ba
Administrator <Administrator@MSDN-SPECIAL>
2014-10-29 01:54:37 +0900

    dfadfafdafadfdwafewa;lhkfl;kjhewqrlk;jhsda hthrewtuh'po wer 'u[otw ret i ew t jouwer jto jo prjog adj o[fads'ojpf

--- Management/src/project.java
+++ Management/src/project.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -5,162 +6,207 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
-class Student implements Serializable{
-  String name;
-  int kor;
-  int eng;
-  int math;
-  int sum;
-  float average;
-  
-  Student(String name, int kor, int eng, int math){
-    this.name = name;
-    this.kor = kor;
-    this.eng = eng;
-    this.math= math;
-  
-    this.sum = kor + eng + math;
-    this.average = sum / 3.0f;
-  }
-  
-  public int hashCode(){
-    return name.hashCode();
-  }
-  
-  public boolean equals(Object obj){
-    Student st = (Student) obj;
-   if(name.compareTo(st.name)==0)
-     return true;
-     else
-     return false;
-  }
+class Student implements Serializable {
+ String name;
+ String kor;
+ String eng;
+ String math;
+ int sum;
+ float average;
+
+ Student(String name, String kor, String eng, String math) {
+  this.name = name;
+  this.kor = kor;
+  this.eng = eng;
+  this.math = math;
+  this.sum = Integer.parseInt(kor) + Integer.parseInt(eng)
+    + Integer.parseInt(math);
+  this.average = sum / 3.0f;
+ }
 }
 
-class StudentManager{
-  
-  private final File dataFile = new File("Score.dat");
-  HashSet<Student> infoStorage = new HashSet<Student>();
-  
-  static StudentManager inst = null;
-  public static StudentManager createManagerInst(){
-    if(inst == null)
-      inst = new StudentManager();
-    return inst;
-  }
-  
-  private StudentManager(){
-    readFromFile();
-  }
-  
-  private Student readStudentInfo(){
-    System.out.println("이름 : ");
-    String name = MenuViewer.keyboard.nextLine();
-    System.out.println("국어 : ");
-    int kor = Integer.parseInt(MenuViewer.keyboard.nextLine());
-    System.out.println("영어 : ");
-    int eng = Integer.parseInt(MenuViewer.keyboard.nextLine());
-    System.out.println("수학 : ");
-    int math = Integer.parseInt(MenuViewer.keyboard.nextLine());
+class StudentManager {
+ private final File datafile = new File("Score.dat");
+ ArrayList<Student> infoStorage = new ArrayList<Student>();
+ static StudentManager inst = null;
+ public static StudentManager createManagerInst() {
+  if (inst == null)
+   inst = new StudentManager();
+  return inst;
+ }
 
-    return new Student(name, kor, eng, math);
+ private StudentManager() {
+  readFromFile();
+ }
+
+ public void inputData(String name, String kor, String eng, String math) {
+  infoStorage.add(new Student(name, kor, eng, math));
+  System.out.println("저장되었습니다.");
+ }
+
+ public void viewInfo(String index) {
+  System.out.println("인덱스: " + Integer.parseInt(index));
+  System.out.println("이름: "
+    + infoStorage.get(Integer.parseInt(index)).name);
+  System.out.println("국어: "
+    + infoStorage.get(Integer.parseInt(index)).kor);
+  System.out.println("영어: "
+    + infoStorage.get(Integer.parseInt(index)).eng);
+  System.out.println("수학: "
+    + infoStorage.get(Integer.parseInt(index)).math);
+  System.out.println("총점: "
+    + infoStorage.get(Integer.parseInt(index)).sum);
+  System.out.println("평균: "
+    + infoStorage.get(Integer.parseInt(index)).average);
+ }
+
+ public void listInfo() {
+  if (infoStorage.size() < 1) {
+   System.out.println("저장된 데이터가 없습니다.");
+  } else {
+   for (int i = 0; i < infoStorage.size(); i++) {
+    System.out.print(i);
+    System.out.print(" " + infoStorage.get(i).name);
+    System.out.print(" " + infoStorage.get(i).kor);
+    System.out.print(" " + infoStorage.get(i).eng);
+    System.out.println(" " + infoStorage.get(i).math);
+   }
   }
+ }
+
+ public void Delete(String index) {
+  if (Integer.parseInt(index) >=0 && Integer.parseInt(index) < infoStorage.size() ) {
+   System.out.print(infoStorage.get(Integer.parseInt(index)).name
+     + "의 성적을 삭제하시겠습니까?(y/n)");
+   
+   String dicision = MenuViewer.keyboard.nextLine();
+   if (dicision.equals("y")   || dicision.equals("Y")) {
+    infoStorage.remove(Integer.parseInt(index));
+    System.out.println("삭제하였습니다.");
+   
+   } else if (dicision.equals("n")   || dicision.equals("N")) {
+    System.out.println("삭제 취소하였습니다.");
+   } 
+  }else
+   System.out.println("존재하지 않는 인덱스입니다.");
+ }
+
+ public void update(String index) {
+  String name = null;
+  String kor = null;
+  String eng = null;
+  String math = null;
+  int no = Integer.parseInt(index);
+
+  System.out.print("이름 :" + "(" + infoStorage.get(no).name + ")");
+  String input = MenuViewer.keyboard.nextLine();
+  if (input.equals(""))
+   name = infoStorage.get(no).name;
+  else
+   name = input;
   
-  public void inputData(){
-    System.out.println("데이터 입력을 시작하세요");
-    
-    Student info = null;
-    
-    info = readStudentInfo();
-    
-    boolean isAdded  = infoStorage.add(info);
-    if(isAdded == true){
-      System.out.println("데이터 입력이 완료되었습니다.\n");
-    }else
-        System.out.println("이미 저장된 데이터 입니다..\n");
-    }
-  
-    
-    public void storeToFile(){
-      
-      
-      try{
-        FileOutputStream file = new FileOutputStream(dataFile);
-        ObjectOutputStream out = new ObjectOutputStream(file);
-        
-       Iterator<Student> itr = infoStorage.iterator();
-       while(itr.hasNext())
-       out.writeObject(itr.next());
-       out.close();
-      }
-      catch(IOException e)
-      {
-        e.printStackTrace();
-      }
-    }
-    
-    public void readFromFile(){
-      if(dataFile.exists() == false)
-        return;
-    
-      try{
-        FileInputStream file = new FileInputStream(dataFile);
-        ObjectInputStream in = new ObjectInputStream(file);
-        
-        while(true){
-          Student info = (Student)in.readObject();
-          if(info == null)
-            break;
-          infoStorage.add(info);
-        }
-      in.close();
-      }
-      catch(IOException e){
-        return;
-      }
-      
-      catch(ClassNotFoundException e){
-        return;
-      }
-    }
+  System.out.print("국어 :" + "(" + infoStorage.get(no).kor + ")");
+   input = MenuViewer.keyboard.nextLine();
+  if (input.equals(""))
+   kor = infoStorage.get(no).kor;
+  else
+   kor = input;
+
+  System.out.print("영어 :" + "(" + infoStorage.get(no).eng + ")");
+   input = MenuViewer.keyboard.nextLine();
+  if (input.equals(""))
+   eng = infoStorage.get(no).eng;
+  else
+   eng = input;
+
+  System.out.print("수학 :" + "(" + infoStorage.get(no).math + ")");
+   input = MenuViewer.keyboard.nextLine();
+  if (input.equals(""))
+   math = infoStorage.get(no).math;
+  else
+   math = input;
+
+  System.out.print("정말 변경하시겠습니까?(y/n)");
+   input = MenuViewer.keyboard.nextLine();
+
+  if (input.equals("y")   || input.equals("Y")) {
+   System.out.println("변경하였습니다.");
+   infoStorage.set(no, new Student(name, kor, eng, math));
+   
+  } else if (input.equals("n")   || input.equals("N")) {
+   System.out.println("변경을 취소하였습니다.");
+  }
+ }
+
+ public void storeToFile() {
+  try {
+   FileOutputStream file = new FileOutputStream(datafile);
+   ObjectOutputStream out = new ObjectOutputStream(file);
+   Iterator<Student> itr = infoStorage.iterator();
+
+   while (itr.hasNext())
+    out.writeObject(itr.next());
+   out.close();
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+ }
+
+ public void readFromFile() {
+  try {
+   FileInputStream file = new FileInputStream(datafile);
+   ObjectInputStream in = new ObjectInputStream(file);
+
+   while (true) {
+    Student info = (Student) in.readObject();
+    if (info == null)
+     break;
+    infoStorage.add(info);
+   }
+   in.close();
+  } catch (IOException e) {
+   return;
+  } catch (ClassNotFoundException e) {
+   return;
+  }
+ }
 }
 
-
-class MenuViewer{
-  
-  public static Scanner keyboard = new Scanner(System.in);
-  
-  public static void showMenu(){
-    System.out.println("1.list");
-    System.out.println("2.view");
-    System.out.println("3.add ");
-    System.out.println("5.update");
-    System.out.println("6.exit");
-    System.out.print("선택 : ");
-      }
+class MenuViewer {
+ public static Scanner keyboard = new Scanner(System.in);
+ public static void showMenu() {
+  System.out.println("list");
+  System.out.println("view 인덱스");
+  System.out.println("add 이름 국어 영어 수학");
+  System.out.println("delete 인덱스");
+  System.out.println("update 인덱스");
+  System.out.println("exit");
+ }
 }
-
 
 public class project {
+ public static void main(String[] args) {
+  StudentManager manager = StudentManager.createManagerInst();
 
-  public static void main(String[] args) {
-    
-    StudentManager manager = StudentManager.createManagerInst();
-    int choice;
-    
-    MenuViewer.showMenu();
-
-    choice = Integer.parseInt(MenuViewer.keyboard.nextLine());
-    
-    switch(choice){
-    case 1 : 
-      manager.inputData();
-      manager.storeToFile();
-      break;
-    }
+  while (true) {
+   System.out.print("명령>");
+   String input = MenuViewer.keyboard.nextLine();
+   String arr[] = input.split(" ");
+   
+   switch (arr[0]) {
+   case "help": MenuViewer.showMenu(); break;
+   case "list":  manager.listInfo(); break;
+   case "view":  manager.viewInfo(arr[1]); break;
+   case "add":   manager.inputData(arr[1], arr[2], arr[3], arr[4]); break;
+   case "delete": manager.Delete(arr[1]); break;
+   case "update":  manager.update(arr[1]); break;
+   case "exit": manager.storeToFile(); return;
+   default : System.out.println("잘못된 명령어입니다."); break;
+   }
   }
-
+ }
 }
