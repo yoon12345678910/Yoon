ab874006eac1cc68cfc49336fa17bdc736bd20d8
Administrator <Administrator@MSDN-SPECIAL>
2014-10-30 00:08:43 +0900

    리펙토링 p54

---
+++ Refactoring/src/Chapter01/Customer.java
@@ -0,0 +1,77 @@
+package Chapter01;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+	private String _name;
+	private Vector _rentals = new Vector();
+
+	public Customer(String name) {
+		_name = name;
+	}
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String statement() {
+		Enumeration rentals = _rentals.elements();
+		String result = getName() + "고객님의 대여 기록\n";
+
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+
+			result += "\t" + each.getMovie().getTitle() + "\t"
+					+ String.valueOf(each.getCharge()) + "\n";
+		}
+		// 푸터 행 추가
+		result += "누적 대여료 : " + String.valueOf(getTotalCharge()) + "\n";
+		result += "적립 포인트 : " + String.valueOf(getTotalFrequentRenterPoints());
+		return result;
+	}
+	private double amountFor(Rental aRental) {
+		return aRental.getCharge();
+	}
+	
+	int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+	
+	// 현재까지 누적된 총 대여료
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
+	
+	public String htmlStatement(){
+		Enumeration rentals = _rentals.elements();
+		String result = "<H1><EM>" + getName() + "고객님의 대여 기록</EM></H1><P>\n";
+		while(rentals.hasMoreElements()){
+			Rental each = (Rental) rentals.nextElement();
+			
+			//모든 대여 비디오 정보와 대여료를 출력
+			result += each.getMovie().getTitle() + ":" + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		
+		//푸터 행 추가
+		result += "<P>누적 대여료 :<EM>" +String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "적립 포인트: <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM><P>";
+		return result;
+	}
+}
