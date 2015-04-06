5d9541706f45c129e497f266e5f7f9245221f724
yoon12345678910 <yji1221@naver.com>
2015-04-07 00:17:28 +0900

    h

---
+++ TopCoder/src/Cryptography.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+
+public class Cryptography {
+	
+//증명	
+public long test(int[] numbers){
+	long ans = 0;
+	
+	for(int i =0; i < numbers.length; i ++){
+		long temp = 1;
+		for(int j = 0; j < numbers.length; j++){
+			if(i == j){
+			temp *=(numbers[j] + 1);
+			}else{
+			temp *= numbers[j];	
+			}
+		}
+		System.out.println(ans + "," + temp);
+		ans = Math.max(ans,temp);
+	}
+	return ans;
+}
+	
+public long encrypt(int[] numbers){
+	int maximum = 1;
+	
+	Arrays.sort(numbers);
+	numbers[0]++;
+	
+	for(int n : numbers){
+		System.out.print(n+",");
+		maximum *= n;
+		System.out.println(maximum);
+	}
+	
+	return maximum;
+}
+
+	public static void main(String[] args) {
+		int[] codeSequence = {4, 3, 2, 5, 1};
+		Cryptography cy = new Cryptography();
+		System.out.println("최댓값" + cy.test(codeSequence));
+		System.out.println("최댓값" + cy.encrypt(codeSequence));
+	}
+
+}
